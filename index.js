2b3b84e673dc1ad7779da3c7a298f8ded8a51b07
igorkukric <you@example.com>
2023-08-20 01:39:06 +0200

    Fetch weather data on document load for default location

--- index.js
+++ index.js
@@ -2,35 +2,35 @@ const apiKey = "4480352f3e978527b52b9e68add6b0e0";
 const locButton = document.querySelector(".loc-button");
 const todayInfo = document.querySelector(".today-info");
 const todayWeatherIcon = document.querySelector(".today-weather i");
-const todayTemp = document.querySelector("weather-temp");
+const todayTemp = document.querySelector(".weather-temp");
 const daysList = document.querySelector(".days-list");
 
 // Mapping of weather condition codes to icon class names (Depending on Openweather Api Response)
 const weatherIconMap = {
-  "01d": "sun",
-  "01n": "moon",
-  "02d": "sun",
-  "02n": "moon",
-  "03d": "cloud",
-  "03n": "cloud",
-  "04d": "cloud",
-  "04n": "cloud",
-  "09d": "cloud-rain",
-  "09n": "cloud-rain",
-  "10d": "cloud-rain",
-  "10n": "cloud-rain",
-  "11d": "cloud-lightning",
-  "11n": "cloud-lightning",
-  "13d": "cloud-snow",
-  "13n": "cloud-snow",
-  "50d": "water",
-  "50n": "water",
+  '01d': 'sun',
+  '01n': 'moon',
+  '02d': 'sun',
+  '02n': 'moon',
+  '03d': 'cloud',
+  '03n': 'cloud',
+  '04d': 'cloud',
+  '04n': 'cloud',
+  '09d': 'cloud-rain',
+  '09n': 'cloud-rain',
+  '10d': 'cloud-rain',
+  '10n': 'cloud-rain',
+  '11d': 'cloud-lightning',
+  '11n': 'cloud-lightning',
+  '13d': 'cloud-snow',
+  '13n': 'cloud-snow',
+  '50d': 'water',
+  '50n' : 'water'
 };
 
 function fetchWeatherData(location) {
   //construct the API url with the location and api key
-  const apiUrl = `api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${apiKey}&units=metric`;
-
+  const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${apiKey}&units=metric`;
+console.log(location)
   // Fetch weather data from api
   fetch(apiUrl)
     .then((response) => response.json())
@@ -39,29 +39,28 @@ function fetchWeatherData(location) {
       const todayWeather = data.list[0].weather[0].description;
       const todayTemperature = `${Math.round(data.list[0].main.temp)}°C`;
       const todayWeatherIconCode = data.list[0].weather[0].icon;
-
-      todayInfo.querySelector("h2").textContent = new Date().toDateString(
-        "en",
-        { weekday: "long" }
+      todayInfo.querySelector('h2').textContent = new Date().toDateString(
+        'en',
+        { weekday: 'long' }
       );
-      todayInfo.querySelector("span").textContent = new Date().toDateString(
-        "en",
-        { day: "numeric", month: "long", year: "numeric" }
+      todayInfo.querySelector('span').textContent = new Date().toDateString(
+        'en',
+        { day: 'numeric', month: 'long', year: 'numeric' }
       );
       todayWeatherIcon.className = `bx bx-${weatherIconMap[todayWeatherIconCode]}`;
       todayTemp.textContent = todayTemperature;
 
       // Update location and weather description in the "left-info" section
       const LocationElement = document.querySelector(
-        ".today-info > div > span"
+        '.today-info > div > span'
       );
       LocationElement.textContent = `${data.city.name}, ${data.city.country}`;
 
       const weatherDescriptionElement = document.querySelector(
-        ".today-weather > h3"
+        '.today-weather > h3'
       );
       weatherDescriptionElement.textContent = todayWeather;
-
+      console.log(data)
       // Update todays info in the "day-info" section
       const todayPrecipitation = `${data.list[0].pop}%`;
       const todayHumidity = `${data.list[0].main.humidity}%`;
@@ -110,8 +109,8 @@ function fetchWeatherData(location) {
           daysList.innerHTML += `
             
                 <li>
-                    <i class='bx bx-${weatherIconMap[iconCode]}'</i>
-                    <span>${dayAbbreviation}{</span>
+                    <i class='bx bx-${weatherIconMap[iconCode]}'></i>
+                    <span>${dayAbbreviation}</span>
                     <span class="day-temp">${dayTemp}
                     </span>
                 </li>    
@@ -120,7 +119,24 @@ function fetchWeatherData(location) {
           count++;
         }
 
-        
+        // Stop after getting 4 distinct days
+        if (count === 4) break;
       }
+    })
+    .catch((error) => {
+      alert(`Error fetching weather data: ${error} (Api Error)`);
     });
 }
+
+// Fetch weather data on document load for default location (Serbia)
+document.addEventListener('DOMContentLoaded' , ()=>{
+    const defaultLocation = 'Serbia'
+    fetchWeatherData(defaultLocation)
+})
+
+locButton.addEventListener('click', ()=>{
+    const location = prompt('Enter a location :')
+    if (!location) return
+
+    fetchWeatherData(location)
+})
